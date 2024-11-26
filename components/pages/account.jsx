b2d009563a26acb20f9c662f947dfefbeca8a108
dynann <dynannran@gmail.com>
2024-11-26 22:41:07 +0700

    change commit

---
+++ components/pages/account.jsx
@@ -0,0 +1,54 @@
+'use client'
+import Link from "next/link";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+export default function AccountPage(){
+    const {data: session} = useSession()
+    return (
+        <div className="bg-white h-screen flex flex-col top-0 ">
+          {session ? (
+            <div>
+              <div className=" bg-primary flex flex-col items-center justify-center space-y-2 pt-18 py-12 rounded-xl ">
+                <div className="bg-white rounded-full overflow-hidden w-36 h-36 border-white">
+                  <Image
+                    src="/components/img/profile.png"
+                    width={500}
+                    height={500}
+                    alt="profile picture"
+                  ></Image>
+                </div>
+                <h1 className="text-2xl font-bold pt-3">Mark Suckerberg</h1>
+              </div>
+    
+              <div className="flex flex-col items-center space-y-4 justify-center mt-10 ">
+                
+                 <div className="flex flex-col bg-accent w-[350px] justify-start rounded-2xl  ">
+                 <p className="text-black font-light text-xs pl-3 pt-3">username</p>
+                 <h1 className="text-black font-medium py-3 pl-3">
+                    {session.user?.name}
+                 </h1>
+                </div>
+                <div className="flex flex-col bg-accent w-[350px] justify-start rounded-2xl  ">
+                 <p className="text-black font-light text-xs pl-3 pt-3">email</p>
+                 <h1 className="text-black font-medium py-3 pl-3">
+                    {session.user?.email}
+                 </h1>
+                </div>
+                <h1 className=" text-secondary  ">
+                Connected Account
+                </h1>
+              </div>
+              <div className="items-center justify-center">
+             
+              </div>
+            
+            </div>
+          ) : (
+            <div>
+              you are not signed in go to sign in page{" "}
+              <Link href="/register"> sign in</Link>
+            </div>
+          )}
+        </div>
+      );
+}
--- components/pages/landing.jsx
+++ components/pages/landing.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <div className=" relative p-8 flex flex-col space-y-4 items-center font-Kulim ">
+    <div className=" relative p-8 flex flex-col space-y-[550px] items-center font-Kulim font-sans">
       <div className="mt-16 flex flex-col items-center">
         <div className="bg-white rounded-full p-4 w-20 h-20 flex items-center justify-center mb-4">
             {/* logo section */}
@@ -20,9 +20,9 @@ export default function LandingPage() {
         <h1 className="text-white text-3xl font-bold">WOWSOON</h1>
         {/* logo section */}
       </div>
-      <div className="bg-white text-gray-800 w-full max-w-xs py-4 px-6 rounded-full  flex items-center justify-center ">
-      <Link href="/register"><button> continue </button></Link>
-      </div>
+     
+      <button className="bg-white text-secondary w-[350px] h-[59px] max-w-xs py-4 rounded-[24px] flex items-center justify-center "><Link href="/register"> Continue</Link> </button>
+     
     </div>
   );
 }
