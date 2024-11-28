d19693252366b54a8a1ed330664550b4329bd4c6
dynann <dynannran@gmail.com>
2024-11-28 02:31:10 +0000

    Update ButtonWithIcon.js

--- components/ButtonWithIcon.js
+++ components/ButtonWithIcon.js
@@ -1,32 +1,36 @@
-import Link from "next/link";
+"use client"
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 function ButtonWithIcon({ label, icon: Icon, route, onClick }) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if(onClick){
+      onClick()
+    } else if(route){
+      router.push(route)
+    }
+  };
+
   return (
-    <div className="relative w-full max-w-xs mb-4 h-full">
-      {route ? (
-        <Link
-          href={route}
-          className="flex items-center bg-accent text-secondary pl-[75px] w-[350px] h-[59px]  py-2 px-14 rounded-[12px] hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-150"
-        >
-          <Icon className="absolute left-6 top-1/2 transform -translate-y-1/2 text-gray-500" size={24} />
-          <span className="absolute left-[62px] top-1/2 transform -translate-y-1/2 text-gray-500">
-            &#8739;
-          </span>
-          {label}
-        </Link>
-      ) : (
-        <button
-          onClick={onClick}
-          className="flex items-center  bg-accent text-secondary pl-[75px] w-[350px] h-[59px]  py-2 px-14 rounded-[12px] hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-primary transition-all duration-150"
-        >
-          <Icon className="absolute left-6 top-1/2 transform -translate-y-1/2 text-gray-500" size={24} />
-          <span className="absolute left-[62px] top-1/2 transform -translate-y-1/2 text-gray-500">
-            &#8739;
-          </span>
-          {label}
-        </button>
+    <button
+      className="relative bg-accent text-secondary font-semibold w-[350px] h-[59px] rounded-2xl flex items-center justify-start px-6"
+      onClick={handleClick}
+    >
+      {/* Render the icon */}
+      {Icon && (
+        <Icon className="absolute left-6 top-1/2 transform -translate-y-1/2 text-gray-500" size={24} />
       )}
-    </div>
+
+      {/* Vertical separator */}
+      <span className="absolute left-[62px] top-1/2 transform -translate-y-1/2 text-gray-500">
+        &#8739;
+      </span>
+
+      {/* Button label */}
+      <span className="pl-[75px]">{label}</span>
+    </button>
   );
 }
 
